42b6182b8572262b82bc32df56e6f3b724a7224e
TahmidShawn7 <tahmidshawn7@gmail.com>
2023-10-20 01:12:46 +0600

    product delete from my cart section done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -52,6 +52,15 @@ async function run() {
             const cursor = products.find();
             const result = await cursor.toArray()
             res.send(result)
+        })
+
+        // delete product from my cart 
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await products.deleteOne(query);
+            res.send(result)
+
         })
 
         // get brand data from mongoDB  
