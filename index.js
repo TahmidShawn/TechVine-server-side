021d5dc17f21dd34aaabc3dc27ad113dbe770e87
TahmidShawn7 <tahmidshawn7@gmail.com>
2023-10-20 19:54:13 +0600

    backend server design completed

--- index.js
+++ index.js
@@ -36,7 +36,56 @@ async function run() {
         const nokia = brandCollection.collection("nokia");
         const samsung = brandCollection.collection("samsung");
         const sony = brandCollection.collection("sony");
+        const lava = brandCollection.collection("lava");
         const products = brandCollection.collection("products");
+
+        // add apple product to route 
+        app.post('/apple', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await apple.insertOne(newProduct);
+            res.send(result);
+        })
+
+        // add samsung product to route 
+        app.post('/samsung', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await samsung.insertOne(newProduct);
+            res.send(result);
+        })
+
+        // add nokia product to route 
+        app.post('/nokia', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await nokia.insertOne(newProduct);
+            res.send(result);
+        })
+
+        // add intel product to route 
+        app.post('/intel', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await intel.insertOne(newProduct);
+            res.send(result);
+        })
+
+        // add sony product to route 
+        app.post('/sony', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await sony.insertOne(newProduct);
+            res.send(result);
+        })
+
+        app.post('/lava', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await lava.insertOne(newProduct);
+            res.send(result);
+        })
+
 
 
         // created add to cart data for mongoDB 
@@ -62,6 +111,189 @@ async function run() {
             res.send(result)
 
         })
+
+        // Update Apple 
+        app.put('/apple/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedBrand = req.body;
+
+            const appleBrand = {
+                $set: {
+                    name: updatedBrand.name,
+                    brandName: updatedBrand.brandName,
+                    type: updatedBrand.type,
+                    price: updatedBrand.price,
+                    rating: updatedBrand.rating,
+                    image: updatedBrand.image,
+
+                }
+            }
+
+            const result = await apple.updateOne(filter, appleBrand, options);
+            res.send(result);
+        })
+
+        // Update Samsung 
+        app.put('/samsung/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedBrand = req.body;
+
+            const samsungBrand = {
+                $set: {
+                    name: updatedBrand.name,
+                    brandName: updatedBrand.brandName,
+                    type: updatedBrand.type,
+                    price: updatedBrand.price,
+                    rating: updatedBrand.rating,
+                    image: updatedBrand.image,
+
+                }
+            }
+
+            const result = await samsung.updateOne(filter, samsungBrand, options);
+            res.send(result);
+        })
+
+        // Update Nokia
+        app.put('/nokia/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedBrand = req.body;
+
+            const nokiaBrand = {
+                $set: {
+                    name: updatedBrand.name,
+                    brandName: updatedBrand.brandName,
+                    type: updatedBrand.type,
+                    price: updatedBrand.price,
+                    rating: updatedBrand.rating,
+                    image: updatedBrand.image,
+
+                }
+            }
+
+            const result = await nokia.updateOne(filter, nokiaBrand, options);
+            res.send(result);
+        })
+
+        // Update sony
+        app.put('/sony/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedBrand = req.body;
+
+            const sonyBrand = {
+                $set: {
+                    name: updatedBrand.name,
+                    brandName: updatedBrand.brandName,
+                    type: updatedBrand.type,
+                    price: updatedBrand.price,
+                    rating: updatedBrand.rating,
+                    image: updatedBrand.image,
+
+                }
+            }
+
+            const result = await sony.updateOne(filter, sonyBrand, options);
+            res.send(result);
+        })
+
+        // Update Intel
+        app.put('/intel/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedBrand = req.body;
+
+            const intelBrand = {
+                $set: {
+                    name: updatedBrand.name,
+                    brandName: updatedBrand.brandName,
+                    type: updatedBrand.type,
+                    price: updatedBrand.price,
+                    rating: updatedBrand.rating,
+                    image: updatedBrand.image,
+
+                }
+            }
+
+            const result = await intel.updateOne(filter, intelBrand, options);
+            res.send(result);
+        })
+
+        app.put('/lava/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedBrand = req.body;
+
+            const lavaBrand = {
+                $set: {
+                    name: updatedBrand.name,
+                    brandName: updatedBrand.brandName,
+                    type: updatedBrand.type,
+                    price: updatedBrand.price,
+                    rating: updatedBrand.rating,
+                    image: updatedBrand.image,
+
+                }
+            }
+
+            const result = await intel.updateOne(filter, lavaBrand, options);
+            res.send(result);
+        })
+
+        // co Route 
+        app.get('/apple/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await apple.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/samsung/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await samsung.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/nokia/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await nokia.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/sony/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await sony.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/intel/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await intel.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/lava/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await lava.findOne(query);
+            res.send(result)
+        })
+
+
+
 
         // get brand data from mongoDB  
         app.get('/brand', async (req, res) => {
@@ -101,6 +333,13 @@ async function run() {
         // get sony data from mongoDB 
         app.get('/sony', async (req, res) => {
             const cursor = sony.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // get lava data from mongoDB 
+        app.get('/lava', async (req, res) => {
+            const cursor = lava.find();
             const result = await cursor.toArray()
             res.send(result)
         })
